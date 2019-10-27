fc673e8643876647d721f3c22316e36baa172804
Daylan <daylandelange@gmail.com>
2019-10-27 18:20:55 +0100

    switched shades

--- Opdracht 2/Lena.c
+++ Opdracht 2/Lena.c
@@ -9,18 +9,18 @@
 #include <stdlib.h>
 #include "lenaArray.h"
 
+double totalGrey;
+double average;
+
 void analyzeLena();
 
 int main () {
     analyzeLena(0,512,0,512);
     analyzeLena(100,400,100,400);
-    analyzeLena(0,512,160,380);
     return 0;
 }
 
 void analyzeLena(int beginY, int endY, int beginX, int endX) {
-    double totalGrey;
-    double average;
     FILE *f  = fopen("Lena.txt", "w"); // open file for later use
     if (f == NULL) {printf("error opening file"); exit(1);} // print error if file is not found
 
@@ -30,16 +30,16 @@ void analyzeLena(int beginY, int endY, int beginX, int endX) {
         {
             totalGrey += lena[i][j];
             // print ASCII value and store it in a file
-            if (lena[i][j] < 50) {printf(" "); fprintf(f, " ");}
-                else if (lena[i][j] < 70) {printf("."); fprintf(f, ".");}
-                else if (lena[i][j] < 90) {printf(":"); fprintf(f, ":");}
-                else if (lena[i][j] < 110) {printf("-"); fprintf(f, "-");}
-                else if (lena[i][j] < 130) {printf("="); fprintf(f, "=");}
-                else if (lena[i][j] < 150) {printf("+"); fprintf(f, "+");}
-                else if (lena[i][j] < 170) {printf("*"); fprintf(f, "*");}
-                else if (lena[i][j] < 190) {printf("#"); fprintf(f, "#");}
-                else if (lena[i][j] < 210) {printf("%%"); fprintf(f, "%%");}
-                else {printf("@"); fprintf(f, "@");}
+            if (lena[i][j] < 50) {printf("@"); fprintf(f, "@");}
+                else if (lena[i][j] < 70) {printf("%%"); fprintf(f, "%%");}
+                else if (lena[i][j] < 90) {printf("#"); fprintf(f, "#");}
+                else if (lena[i][j] < 110) {printf("*"); fprintf(f, "*");}
+                else if (lena[i][j] < 130) {printf("+"); fprintf(f, "+");}
+                else if (lena[i][j] < 150) {printf("="); fprintf(f, "=");}
+                else if (lena[i][j] < 170) {printf("-"); fprintf(f, "-");}
+                else if (lena[i][j] < 190) {printf(":"); fprintf(f, ":");}
+                else if (lena[i][j] < 210) {printf("."); fprintf(f, ".");}
+                else {printf(" "); fprintf(f, " ");}
         }
         // enter after each column
         printf("\n");
